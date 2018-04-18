dd045cff9b1feb009fe11a0ab74caee013b8c5ae
Cabrel Ngamaleu <35937353+cabrelngamaleu@users.noreply.github.com>
2018-04-18 07:21:30 +0100

    Add files via upload

---
+++ shoppingbackend/src/main/java/net/cabrel/shoppingbackend/dao/CategoryDAO.java
@@ -0,0 +1,18 @@
+package net.cabrel.shoppingbackend.dao;
+
+import java.util.List;
+
+import net.cabrel.shoppingbackend.dto.Category;
+
+public interface CategoryDAO {
+
+	
+	
+	Category get(int id);
+	List<Category> list();
+	boolean add(Category category);
+	boolean update(Category category);
+	boolean delete(Category category);
+	
+	
+}
